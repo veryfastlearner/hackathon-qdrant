d48f878bcd73413d66afeee01102b050ae0d7265
veryfastlearner <ahmed.benmim@supcom.tn>
2026-01-30 22:43:53 +0000

    final update

--- chunkdata.py
+++
@@ -1,6 +0,0 @@
-from chonkie import Chunkie
-loan_app=""
-chunker = Chunkie(chunk_size=200)
-
-# Get chunks
-chunks = chunker.chunk(loan_app)
--- qdrant.py
+++
@@ -1,4 +0,0 @@
-from qdrant_client import QdrantClient, models
-
-client = QdrantClient(url="localhost", port=6333)
-collection = client.get_collection("credit_apps")
